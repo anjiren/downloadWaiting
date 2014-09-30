2f8bd1cd3e27b27d15ef1b4acab1ea0eebc9229d
Anji Ren <aren@mit.edu>
2014-09-30 11:42:52 -0400

    Download extension without popup tracking download frequency and times

--- js/background.js
+++ js/background.js
@@ -1,8 +1,18 @@
 Parse.initialize("TbO2JRJP6Z5JR7SzcP0BZ08Kx7bViSFqsdsG7VxA", "TYzfDGZzEP8F5U4YDXujM5zPRbAxy1YHMei1T6CG");
+// Helper function for generating alphanumeric string.
+function randomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;
+}
+var randomID = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 
-
+chrome.storage.sync.set({'USER_ID': randomID}, function() {
+          // Notify that we saved.
+          console.log("User ID " + randomID + " generated and saved.");
+        });
 function triggerLearning(reason) {
-  // Send a message to the active tab to launch a popup
+  // Send a message to the active tab to launch a popup.
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { greeting: "launchPopup" },
     function(response) {});
@@ -11,7 +21,9 @@ function triggerLearning(reason) {
 
 /* Checks for downloads. Currently triggers learning only on large downloads. */
 chrome.downloads.onCreated.addListener(function(downloadItem) {
-
+  //chrome.storage.sync.get('USER_ID', function(obj) {
+    //console.log(obj) 
+  //});
   console.log("DownloadWaiting Extension: Downloading...");
   var downloadSize = downloadItem.fileSize;
   console.log("DownloadWaiting Extension: Download size: " + downloadSize + "b"); 
@@ -24,31 +36,64 @@ chrome.downloads.onCreated.addListener(function(downloadItem) {
     dataChunk.set("tabID", tabs[0].id);
   });
 
+  chrome.storage.sync.get('USER_ID', function(obj) {
+    dataChunk.set("userID", obj.USER_ID);
+  })
   dataChunk.set("size", downloadSize);
   dataChunk.set("downloadStartTime", downloadItem.startTime);
-  dataChunk.set("downloadEstimatedEndTime", downloadItem.estimatedEndTime);
+  // trigglerLearning("download");
+
+ var waitForDownloadComplete = setInterval(function(){
+      chrome.downloads.search({}, function(items){
+          var item = items[items.length - 1];
+          if (item.state == "in_progress") {
+            console.log("% Downloaded: " + item.bytesReceived + " " + item.bytesReceived/item.totalBytes);
+            console.log("Estimated end time: " + item.estimatedEndTime);
+          } else if (item.state == "complete") {
+            clearInterval(waitForDownloadComplete);
+            console.log("DownloadWaiting: Download complete!");
+            console.log("DownloadWaiting: Download end time: " + item.endTime);
+            dataChunk.set("downloadEndTime", item.endTime)
+            console.log(item.id);
+              dataChunk.save(null, {
+              success: function(gameScore) {
+                console.log("DownloadWaiting: Data sent to Parse.")
+              },
+              error: function(gameScore, error) {
+                console.log("DownloadWaiting: Parse save error.");
+              }
+              });
+          } else {
+            // Download was interrupted.
+            clearInterval(waitForDownloadComplete);
+          }
+      });
+    }, 2000);
 
 
-  // while (downloadItem.state === "in_progress") {
-  // }
-
-  // if (downloadItem.state === "complete") {
-    dataChunk.set("downloadEndTime", downloadItem.endTime);
-  // }
-
-  dataChunk.save(null, {
-  success: function(gameScore) {
-    // Execute any logic that should take place after the object is saved.
-    //alert('New download data object created with ID: ' + dataChunk.id);
-  },
-  error: function(gameScore, error) {
-    // Execute any logic that should take place if the save fails.
-    // error is a Parse.Error with an error code and message.
-    //alert('Failed to create new download data object, with error code: ' + error.message);
-  }
-  });
-
-  console.log("Data sent to Parse.")
+ // var waitForDownloadComplete = setInterval(function(){
+ //      chrome.downloads.search({}, function(items){
+ //        items.forEach(function(item){
+ //          if (item.state == "in_progress") {
+ //            console.log("% Downloaded: " + item.bytesReceived + " " + item.bytesReceived/item.totalBytes);
+ //            console.log("Estimated end time: " + item.estimatedEndTime);
+ //          } else if (item.state == "complete") {
+ //            clearInterval(waitForDownloadComplete);
+ //            console.log("Download complete!");
+ //            dataChunk.set("downloadEndTime", downloadItem.endTime)
+ //            console.log(item.id);
+ //              dataChunk.save(null, {
+ //              success: function(gameScore) {
+ //                console.log("Data sent to Parse.")
+ //              },
+ //              error: function(gameScore, error) {
+ //                console.log("Parse save error.");
+ //              }
+ //              });
+ //          }
+ //        })
+ //      });
+ //    }, 2000);
 
     // Incrementally checks progress of download
    /* setInterval(function(){
